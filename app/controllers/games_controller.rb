3daf778b602292c359386629342cb2b830abd146
danny galimi <dannygalimi13@aim.com>
2022-07-30 12:55:21 -0400

    create games_controller create action that renders game show page if successful and game new page if not successful

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -4,4 +4,14 @@ class GamesController < ApplicationController
         @game = Game.new
     end 
 
+    def create
+        @game = Game.new(game_params)
+        @game.user_id = session[:user_id]
+        if @game.save
+            redirect_to game_path(@game)
+        else
+            render :new
+        end 
+    end
+
 end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_many :reviews
-    has_many :guitars
+    has_many :games
     
     validates :username, uniqueness: true, presence: true
     has_secure_password
