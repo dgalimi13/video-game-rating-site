6754194e46dbe89d2683146d5f086e3de013652a
danny galimi <dannygalimi13@aim.com>
2022-07-31 10:26:13 -0400

    add @game.build_console line to games_controller create action

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -18,6 +18,7 @@ class GamesController < ApplicationController
             redirect_to game_path(@game)
         else
             render :new
+            @game.build_console
         end 
     end
 
