2e33439026e21566dbe4c9d005d0388ad20111cb
danny galimi <dannygalimi13@aim.com>
2022-07-31 14:17:51 -0400

    add not_a_duplicate method to Game model to ensure same game is not created for a console, add validation for not_a_duplicate methodto Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,8 +2,14 @@ class Game < ApplicationRecord
   belongs_to :console
   belongs_to :user
   validates :name, presence: true
+  validates :not_a_duplicate
   # has_many :reviews
   # has_many :users, through: :reviews
+
+  def not_a_duplicate
+    if Game.find_by(name: name, console_id: console_id)
+      errors.add(:name, "has already been added for that make")
+  end 
 
   def console_attributes=(attributes)
     self.console = Console.find_or_create_by(attributes) if !attributes['name'].empty?
