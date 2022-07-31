073165b2dc8b92d2735d68237d5f43d27fac0824
danny galimi <dannygalimi13@aim.com>
2022-07-31 13:01:13 -0400

    add link to app/views/games/show.html.erb to games edit page only if it the current user

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -29,7 +29,7 @@ class GamesController < ApplicationController
     end 
 
     def update 
-        if @guitar.update(game_params)
+        if @game.update(game_params)
             redirect_to game_path(@game)
         else
             render :edit 
