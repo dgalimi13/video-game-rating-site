6711362893cbf3349bbe9098196f24ab12201cb9
danny galimi <dannygalimi13@aim.com>
2022-08-01 10:52:59 -0400

    add code to display_header method in app/helpers/reviews_helper.rb to render an h1 tag with add review message for the games name and consle if params[:game_id] exists or create a new review if not

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,5 +1,11 @@
 module ReviewsHelper 
-
-
-
+    
+    def display_header(review)
+        if params[:guitar_id]
+            content_tag(:h1, "Add a Review for #{review.game.name} - #{review.guitar.console.name}")
+        else
+            content_tag(:h1, "Create a review")
+        end
+    end 
+    
 end 
