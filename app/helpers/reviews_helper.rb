c93acea2a92fdfde25b47126c483ec8de89c5d52
danny galimi <dannygalimi13@aim.com>
2022-08-01 11:04:57 -0400

    fix typos in app/helpers/reviews_helper and new review form

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -2,7 +2,7 @@ module ReviewsHelper
     
     def display_header(review)
         if params[:game_id]
-            content_tag(:h1, "Add a Review for #{review.game.name} - #{review.guitar.console.name}")
+            content_tag(:h1, "Add a Review for #{review.game.name} - #{review.game.console.name}")
         else
             content_tag(:h1, "Create a review")
         end
