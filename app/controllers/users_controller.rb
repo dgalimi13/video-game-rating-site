84506dbf148e80abbd5bf7e47c16099c947dfb16
danny galimi <dannygalimi13@aim.com>
2022-08-01 13:33:22 -0400

    add render call to layouts/_errors partial in users new form, pass @user as object, change flash message and redirect in users controller to :new

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,7 @@ class UsersController < ApplicationController
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else 
-            flash[:error] = "Sorry, Register info was invalid. Please try again"
-            redirect_to signup_path
+            render :new
             
         end 
     end 
