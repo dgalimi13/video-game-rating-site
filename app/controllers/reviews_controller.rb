f27997ab1056cddf0f86070c33be11979e11841e
danny galimi <dannygalimi13@aim.com>
2022-08-01 12:34:58 -0400

    add create action to reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,6 +8,15 @@ class ReviewsController < ApplicationController
         end 
     end 
 
+    def create
+        @review = current_user.reviews.build(review_params)
+        if @review.save
+            redirect_to review_path(@review)
+        else
+            render :new
+        end 
+    end 
+
 
 
 end 
