b342fdbf6fb9372c2ab82cbe9e4841f85104b171
danny galimi <dannygalimi13@aim.com>
2022-08-01 13:05:22 -0400

    correct typo in review model from :guitar to :game validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -5,5 +5,5 @@ class Review < ApplicationRecord
   validates :title, presence: true
   validates :stars, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 6}
 
-  validates :guitar, uniqueness: { scope: :user, message: "has already been reviewed by you"  }
+  validates :game, uniqueness: { scope: :user, message: "has already been reviewed by you"  }
 end
